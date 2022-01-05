7d6ae0b872fd88678cd35976a13db47fcae576f8
dennimir <dennimir@gmail.com>
2022-01-05 12:47:13 +0200

    findpivotindex solution

--- src/main/java/leetcode/findpivotindex/App.java
+++ src/main/java/leetcode/findpivotindex/App.java
@@ -2,4 +2,10 @@ package leetcode.findpivotindex;
 
 public class App
 {
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = {2,0,1,1};
+        System.out.println(solution.pivotIndex(nums));
+    }
 }
